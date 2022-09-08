abe3adbf1314dde137635846bcd258b8f854db53
Julia Popova <yuliapopova.s@yandex.ru>
2022-09-08 13:47:30 +0300

    bug fixing

--- src/components/elements/basketList/BasketList.js
+++ src/components/elements/basketList/BasketList.js
@@ -5,22 +5,24 @@ function BasketList({ products, onDelete, onGetAmountOfOrder }) {
   return (
     <BasketListWrap>
       <div className="container">
-        {products.map((key) => {
-          const { id, img, name, price, quantity } = key;
-          return (
-            <CardForBasket
-              key={id}
-              img={img}
-              name={name}
-              price={price * quantity}
-              onDelete={() => onDelete(id)}
-              onGetAmountOfOrder={() => {
-                onGetAmountOfOrder(id);
-              }}
-              quantity={quantity}
-            />
-          );
-        })}
+        <div className="basket-list">
+          {products.map((key) => {
+            const { id, img, name, price, quantity } = key;
+            return (
+              <CardForBasket
+                key={id}
+                img={img}
+                name={name}
+                price={price * quantity}
+                onDelete={() => onDelete(id)}
+                onGetAmountOfOrder={() => {
+                  onGetAmountOfOrder(id);
+                }}
+                quantity={quantity}
+              />
+            );
+          })}
+        </div>
       </div>
     </BasketListWrap>
   );
--- src/components/elements/basketList/BasketListWrap.styled.js
+++ src/components/elements/basketList/BasketListWrap.styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 const BasketListWrap = styled.section`
   width: 100%;
   padding-bottom: 77.5px;
+  height: calc(773px - 113.5px - 90px);
   overflow-x: auto;
-  max-height: calc(100vh - 113.5px - 90px);
 
   ::-webkit-scrollbar {
     width: 8px;
