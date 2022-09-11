773cd23fcc24336959585e5a381459e55af3111e
Julia Popova <yuliapopova.s@yandex.ru>
2022-09-11 20:01:20 +0300

    bug fixing layout

--- src/components/elements/basketList/BasketListWrap.styled.js
+++ src/components/elements/basketList/BasketListWrap.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 const BasketListWrap = styled.section`
   .basket-list {
     width: 100%;
-    height: calc(73vh - 113.5px - 90px);
+    height: calc(100vh - 113.5px - 90px);
     overflow-x: auto;
 
     ::-webkit-scrollbar {
@@ -22,7 +22,7 @@ const BasketListWrap = styled.section`
   }
   .container {
     max-width: 920px;
-    padding: 0 10px 0 30px;
+    padding: 0 5px 0 30px;
     margin: 0 auto;
   }
 `;
