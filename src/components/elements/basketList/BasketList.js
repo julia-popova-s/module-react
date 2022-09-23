b45e99b3cfeac7ea38d0cf73b47fd16e4f911f67
Julia Popova <yuliapopova.s@yandex.ru>
2022-09-23 18:53:32 +0300

    added message about an empty cart

--- src/components/elements/basketList/BasketList.js
+++ src/components/elements/basketList/BasketList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { CardBasket } from "../cardBasket";
 import { BasketListWrap } from "./BasketListWrap.styled";
 
@@ -6,19 +8,29 @@ export function BasketList({ products }) {
     <BasketListWrap>
       <div className="container">
         <div className="basket-list">
-          {products.map((key) => {
-            const { id, img, name, price, quantity } = key;
-            return (
-              <CardBasket
-                key={id}
-                id={id}
-                img={img}
-                name={name}
-                price={price}
-                quantity={quantity}
-              />
-            );
-          })}
+          {products.length === 0 ? (
+            <div className="basket-list__alert">
+              {"В корзине пока пусто. Загляните на"}
+              <Link to={"/"} className="basket-list__link">
+                {" главную"}
+              </Link>
+              {", чтобы выбрать товары"}
+            </div>
+          ) : (
+            products.map((key) => {
+              const { id, img, name, price, quantity } = key;
+              return (
+                <CardBasket
+                  key={id}
+                  id={id}
+                  img={img}
+                  name={name}
+                  price={price}
+                  quantity={quantity}
+                />
+              );
+            })
+          )}
         </div>
       </div>
     </BasketListWrap>
