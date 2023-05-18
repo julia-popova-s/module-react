3bebff5c20b8c409f31854297ef14109f5614f05
Julia Popova <yuliapopova.s@yandex.ru>
2023-05-18 17:04:02 +0300

    fixed image paths

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './store/'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/module_react">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
